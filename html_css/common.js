2a33a9987dfac1df4088faab931295f42b4db089
webpg11month <webprogrammermash@gmail.com>
2021-02-04 13:33:30 +0900

    edit_manshou

--- html_css/common.js
+++ html_css/common.js
@@ -7,10 +7,18 @@ $(function(){
     });
     function alertFunc(){
         if ($(this).css('display') === 'block') {
-          $(".switch-btn").html("<img id='fadeInOutImg' src='batsu.png' alt='検索'>");
+          $(".switch-btn").html("<i class='fa fa-times size' id='fadeInOutImg' aria-hidden='false'></i>");
         }else{
           $(".switch-btn").html("<i class='fa fa-search size' id='fadeInOutImg' aria-hidden='true'></i>");
         }
       };
     });   
   });
+  $('.hamburger-open').click(function() {
+    //属性の削除
+    $("#fadeInOutImg").remove();
+  })
+  $('.hamburger-close').click(function() {
+    //属性の削除
+    $(".switch-btn").html("<i class='fa fa-search size' id='fadeInOutImg' aria-hidden='true'></i>");
+  })
